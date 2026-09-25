5f8e2a0b8f5c186e7392ca514edc38964aa0df4f
wimmeke <64282079+wimmeke@users.noreply.github.com>
2026-09-25 15:41:04 +0200

    Add files via upload

---
+++ dranken.js
@@ -0,0 +1,278 @@
+const drankData = [
+  {
+    "id": "van-t-vat",
+    "naam": "Van ’t vat",
+    "dranken": [
+      {
+        "id": "cristal-boerke",
+        "naam": "Cristal boerke"
+      },
+      {
+        "id": "cristal-ribbelke",
+        "naam": "Cristal ribbelke"
+      }
+    ]
+  },
+  {
+    "id": "bieren",
+    "naam": "Bieren",
+    "dranken": [
+      {
+        "id": "carlsberg",
+        "naam": "Carlsberg"
+      },
+      {
+        "id": "desperados",
+        "naam": "Desperados"
+      },
+      {
+        "id": "duvel",
+        "naam": "Duvel"
+      },
+      {
+        "id": "kasteelbier-rouge",
+        "naam": "Kasteelbier Rouge"
+      },
+      {
+        "id": "kriek",
+        "naam": "Kriek"
+      },
+      {
+        "id": "la-chouffe",
+        "naam": "La Chouffe"
+      },
+      {
+        "id": "omer",
+        "naam": "Omer"
+      },
+      {
+        "id": "tripel-karmeliet",
+        "naam": "Tripel Karmeliet"
+      },
+      {
+        "id": "vedett",
+        "naam": "Vedett"
+      }
+    ]
+  },
+  {
+    "id": "nul-nul-procent",
+    "naam": "0/0 Procent",
+    "dranken": [
+      {
+        "id": "cristal-0-0",
+        "naam": "Cristal 0/0"
+      },
+      {
+        "id": "affligem-0-0",
+        "naam": "Affligem 0/0"
+      }
+    ]
+  },
+  {
+    "id": "frisdranken",
+    "naam": "Frisdranken",
+    "dranken": [
+      {
+        "id": "agrum",
+        "naam": "Agrum"
+      },
+      {
+        "id": "appelsap",
+        "naam": "Appelsap"
+      },
+      {
+        "id": "aquarius-orange",
+        "naam": "Aquarius Orange"
+      },
+      {
+        "id": "aquarius-lemon",
+        "naam": "Aquarius Lemon"
+      },
+      {
+        "id": "aquarius-red",
+        "naam": "Aquarius Red"
+      },
+      {
+        "id": "chocomelk",
+        "naam": "Chocomelk"
+      },
+      {
+        "id": "cola",
+        "naam": "Cola"
+      },
+      {
+        "id": "cola-zero",
+        "naam": "Cola Zero"
+      },
+      {
+        "id": "fanta",
+        "naam": "Fanta"
+      },
+      {
+        "id": "fristi",
+        "naam": "Fristi"
+      },
+      {
+        "id": "ice-tea",
+        "naam": "Ice Tea"
+      },
+      {
+        "id": "ice-tea-green",
+        "naam": "Ice Tea Green"
+      },
+      {
+        "id": "ice-tea-zero",
+        "naam": "Ice Tea Zero"
+      },
+      {
+        "id": "tonic",
+        "naam": "Tonic"
+      },
+      {
+        "id": "water-bruis",
+        "naam": "Water Bruis"
+      },
+      {
+        "id": "water-plat",
+        "naam": "Water Plat"
+      }
+    ]
+  },
+  {
+    "id": "wijnen",
+    "naam": "Wijnen",
+    "dranken": [
+      {
+        "id": "cava-glas",
+        "naam": "Cava Glas"
+      },
+      {
+        "id": "cava-fles",
+        "naam": "Cava Fles"
+      },
+      {
+        "id": "rode-wijn-glas",
+        "naam": "Rode Wijn Glas"
+      },
+      {
+        "id": "rode-wijn-fles",
+        "naam": "Rode Wijn Fles"
+      },
+      {
+        "id": "rose-wijn-glas",
+        "naam": "Rosé Wijn Glas"
+      },
+      {
+        "id": "rose-wijn-fles",
+        "naam": "Rosé Wijn Fles"
+      },
+      {
+        "id": "witte-wijn-glas",
+        "naam": "Witte Wijn Glas"
+      },
+      {
+        "id": "witte-wijn-fles",
+        "naam": "Witte Wijn Fles"
+      }
+    ]
+  },
+  {
+    "id": "warme-dranken",
+    "naam": "Warme dranken",
+    "dranken": [
+      {
+        "id": "koffie-klein",
+        "naam": "Koffie Klein"
+      },
+      {
+        "id": "koffie-groot",
+        "naam": "Koffie Groot"
+      },
+      {
+        "id": "koffie-deca-klein",
+        "naam": "Koffie Deca Klein"
+      },
+      {
+        "id": "koffie-deca-groot",
+        "naam": "Koffie Deca Groot"
+      },
+      {
+        "id": "thee-klein",
+        "naam": "Thee Klein"
+      },
+      {
+        "id": "thee-groot",
+        "naam": "Thee Groot"
+      },
+      {
+        "id": "chocomelk-warm",
+        "naam": "Chocomelk"
+      },
+      {
+        "id": "soep-pakje-curry",
+        "naam": "Soep Pakje Curry"
+      },
+      {
+        "id": "soep-pakje-gevogelte",
+        "naam": "Soep Pakje Gevogelte"
+      },
+      {
+        "id": "soep-pakje-erwten",
+        "naam": "Soep Pakje Erwten"
+      },
+      {
+        "id": "soep-pakje-tomaten",
+        "naam": "Soep Pakje Tomaten"
+      }
+    ]
+  },
+  {
+    "id": "apero",
+    "naam": "Apero",
+    "dranken": [
+      {
+        "id": "martini-wit",
+        "naam": "Martini Wit"
+      },
+      {
+        "id": "martini-rood",
+        "naam": "Martino Rood"
+      },
+      {
+        "id": "porto",
+        "naam": "Porto"
+      },
+      {
+        "id": "jenever",
+        "naam": "Jenever"
+      }
+    ]
+  },
+  {
+    "id": "chips",
+    "naam": "Chips",
+    "dranken": [
+      {
+        "id": "chips-zout",
+        "naam": "Chips Zout"
+      },
+      {
+        "id": "chips-paprika",
+        "naam": "Chips Paprika"
+      },
+      {
+        "id": "chips-peper-zout",
+        "naam": "Chips Peper & Zout"
+      },
+      {
+        "id": "chips-pickles",
+        "naam": "Chips Pickles"
+      },
+      {
+        "id": "grills",
+        "naam": "Grills"
+      }
+    ]
+  }
+];
